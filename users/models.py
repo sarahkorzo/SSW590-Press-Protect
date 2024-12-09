9a3722248a229b809adca304eeeb3fe0b8b7de04
moyosolao <153024668+moyosolao@users.noreply.github.com>
2024-12-08 20:20:50 -0500

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from auditlog.models import AuditlogHistoryField
+from auditlog.registry import auditlog
 
-# Create your models here.
+class NewsItem(models.Model):
+  title = models.CharField(max_length=255)
+  content = models.TextField()
+  embargo_date = models.DateTimeField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  history = AuditlogHistoryField()
+
+auditlog.register(NewsItem)
+
