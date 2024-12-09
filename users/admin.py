e1470398a7ca35fecc7646b938ebb0905d506b96
moyosolao <153024668+moyosolao@users.noreply.github.com>
2024-12-08 20:39:32 -0500

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from auditlog.models import LogEntry
 from .models import CustomUser
 
 #username is admin, pass is test123
@@ -20,5 +21,22 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ('email', 'username', 'role')
     ordering = ('email',)
+    
+    # Admin action to view audit logs for a selected user
+    @admin.action(description='View Audit Logs')
+    def view_audit_logs(self, request, queryset):
+    for user in queryset:
+        logs = LogEntry.objects.filter(actor_object_id=user.id).order_by('-timestamp')
+        if logs.exists():
+            self.message_user(request, f"Audit logs for {user.username}:")
+            for log in logs:
+                self.message_user(
+                    request,
+                    f"Action" {log.get_action_display()}, Date: {log.timestamp}, Changes: {log.changes}"
+                )
+        else:
+            self.message_user(request, f"No audit logs found for {user.username}.")
+
+    actions = ['view_audit_logs'] 
 
 admin.site.register(CustomUser, CustomUserAdmin)
